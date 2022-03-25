d052069e27c9ea73dec70d5f6aa738775ea20cd9
Dipesh Trikam <dipesht@datacom.co.nz>
2022-03-26 11:17:17 +1300

    updated script

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const onProgress = (event) => {
   const progressBar = event.target.querySelector('.progress-bar');
   const updatingBar = event.target.querySelector('.update-bar');
-  const arButton = event.target.querySelector('.ar-button');
+  const arButton = document.getElementById('ar-button');
   updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
   if (event.detail.totalProgress === 1) {
     progressBar.classList.add('hide');
