3d4049608f00224a2b0a60ec152f209b9371e8f4
Dipesh Trikam <dipesht@datacom.co.nz>
2022-05-05 09:24:17 +1200

    Added code to determine if device is iOS or Android

--- script.js
+++ script.js
@@ -27,24 +27,61 @@ document.onreadystatechange = () => {
 
 // Selects a random 3D model to be displayed within model-viewer window
 
-const astroRobotModel = "https://foundryar.blob.core.windows.net/datacom-qr-code/astronaut_avatar.glb"
-const iceCreamManModel = "https://foundryar.blob.core.windows.net/datacom-qr-code/icecream_avatar.glb"
+const astroRobotModel =
+  'https://foundryar.blob.core.windows.net/datacom-qr-code/astronaut_avatar.glb';
+const iceCreamManModel =
+  'https://foundryar.blob.core.windows.net/datacom-qr-code/icecream_avatar.glb';
 
-const lunarRoverIosModel = "https://foundryar.blob.core.windows.net/datacom-qr-code/LunarRover_English.reality"
-const habIosModel = "https://foundryar.blob.core.windows.net/datacom-qr-code/hab_en.reality"
-const cosmonautIosModel = "https://foundryar.blob.core.windows.net/datacom-qr-code/CosmonautSuit_en.reality"
-const iceCreamManIosModel = "https://foundryar.blob.core.windows.net/datacom-qr-code/high_poly_icecream_avatar.usdz"
+const lunarRoverIosModel =
+  'https://foundryar.blob.core.windows.net/datacom-qr-code/LunarRover_English.reality';
+const habIosModel =
+  'https://foundryar.blob.core.windows.net/datacom-qr-code/hab_en.reality';
+const cosmonautIosModel =
+  'https://foundryar.blob.core.windows.net/datacom-qr-code/CosmonautSuit_en.reality';
+const iceCreamManIosModel =
+  'https://foundryar.blob.core.windows.net/datacom-qr-code/high_poly_icecream_avatar.usdz';
 
-const array = [astroRobotModel, iceCreamManModel]
-const arrayIos = [lunarRoverIosModel, habIosModel, cosmonautIosModel, iceCreamManIosModel]
+const array = [astroRobotModel, iceCreamManModel];
+const arrayIos = [
+  lunarRoverIosModel,
+  habIosModel,
+  cosmonautIosModel,
+  iceCreamManIosModel,
+];
 
-const chooseModel = array[getRandomInt(2)]
-const chooseIosModel = arrayIos[getRandomInt(4)]
+const chooseModel = array[getRandomInt(2)];
+const chooseIosModel = arrayIos[getRandomInt(4)];
 
 let elem = document.getElementById('model-viewer');
-elem.src = chooseModel;
-elem['ios-src'] = chooseIosModel;
+elem.src =
+  getMobileOperatingSystem() == 'Android' ? chooseModel : chooseIosModel;
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
+}
+
+/**
+ * Determine the mobile operating system.
+ * This function returns one of 'iOS', 'Android', 'Windows Phone', or 'unknown'.
+ *
+ * @returns {String}
+ */
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  // Windows Phone must come first because its UA also contains "Android"
+  if (/windows phone/i.test(userAgent)) {
+    return 'Windows Phone';
+  }
+
+  if (/android/i.test(userAgent)) {
+    return 'Android';
+  }
+
+  // iOS detection from: http://stackoverflow.com/a/9039885/177710
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return 'iOS';
+  }
+
+  return 'unknown';
 }
